5433776e034825503258cec32e93fe4f152c21d5
Phat117 <ntphat110705@gmail.com>
2026-01-25 23:13:59 +0700

    Message Container

--- src/components/chat/ChatLayout.tsx
+++ src/components/chat/ChatLayout.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import type { Layout } from "react-resizable-panels";
 import Sidebar from "../Sidebar";
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "../ui/resizable";
+import MessageContainer from "./MessageContainer";
 
 interface ChatLayoutProps  {
     defaultLayout: number[] | undefined;
@@ -58,12 +59,14 @@ const ChatLayout = ({defaultLayout=[320,480]}: ChatLayoutProps ) => {
             minSize={"30%"}
 
         >
-            <div className="flex justify-center items-center h-full w-full px-10">
+            {/* <div className="flex justify-center items-center h-full w-full px-10">
                 <div className="flex flex-col justify-center items-center gap-4">
                     <img src="/logo.png" alt="logo" className="w-full md:w-2/3 lg:w-1/2" />
                     <p className="text-muted-foreground text-center">Click on a chat to view the messages</p>
                 </div>
-            </div>
+            </div> */}
+
+            <MessageContainer/>
         </ResizablePanel>
     </ResizablePanelGroup>
   )
