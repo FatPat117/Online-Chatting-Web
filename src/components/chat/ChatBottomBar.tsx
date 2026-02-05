e17d1bc3a0bed1dfea7980f8ea8454c37df87b6a
Phat117 <ntphat110705@gmail.com>
2026-02-05 20:30:16 +0700

    Chat Bottom Bar

--- src/components/chat/ChatBottomBar.tsx
+++ src/components/chat/ChatBottomBar.tsx
@@ -1,19 +1,22 @@
 'use client'
 import { AnimatePresence, motion } from "framer-motion"
-import { ImageIcon } from "lucide-react"
+import { ImageIcon, Loader, SendHorizontal, ThumbsUp } from "lucide-react"
 import { useRef, useState } from "react"
+import { Button } from "../ui/button"
 import { Textarea } from "../ui/textarea"
 import EmojiPicker from "./EmojiPicker"
 
 const ChatBottomBar = () => {
     const [message,setMessage] = useState('')
     const inputRef = useRef<HTMLTextAreaElement>(null)
+    const isPending = false;
   return (
     <div className="p-2 flex justify-between w-full items-center gap-2">
       {!message.trim() && <ImageIcon size={20} className="cursor-pointer text-muted-foreground"/>}
 
       <AnimatePresence>
         <motion.div
+            key="chat-input"
             layout
             initial={{opacity:0,scale:1}}
             animate={{opacity:1,scale:1}}
@@ -46,6 +49,29 @@ const ChatBottomBar = () => {
               />
             </div>
         </motion.div>
+
+            {message.trim()
+              ? (
+                <Button
+                  key="send-button"
+                  className="size-9 dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white shrink-0"
+                  variant={'ghost'}
+                  size={'icon'}
+                >
+                  <SendHorizontal size={20} className="text-muted-foreground" />
+                </Button>
+              ) : (
+                <Button
+                  key="like-button"
+                  className="size-9 dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white shrink-0"
+                  variant={'ghost'}
+                  size={'icon'}
+                >
+                 {!isPending &&  <ThumbsUp size={20} className="text-muted-foreground" />}
+                {isPending &&  <Loader size={20} className="animate-spin" />}
+                </Button>
+              )
+            }
       </AnimatePresence>
     </div>
   )
